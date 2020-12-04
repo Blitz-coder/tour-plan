12ac94eeece5801093bb59f3f7c8b92628fe3233
Blitz-coder <blitz-coder@narod.ru>
2020-12-04 22:43:19 +0700

    create: modal form

--- js/main.js
+++ js/main.js
@@ -1,34 +1,69 @@
-var hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
+  var hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
 
-  effect: "fade",
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
 
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
+    effect: "fade",
 
-var reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-});
+  var reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener('click', function () {
-  console.log('Клик по кнопке');
-  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+  });
+
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener('click', function () {
+    document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  var closeModalOverlay = $(".modal__overlay");
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+  closeModalOverlay.on('click', closeModal);
+
+  //  Убрать модальное окно клавишей Esc
+  $(document).keydown(function (eventObject) {
+    if (eventObject.which == 27) {
+      console.log("клавиша Esc");
+      closeModal(event);
+    };
+  });
+
+
+  function openModal() {
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
+
+
 });
